47db71d269ce3f3fabc02373f53788d20a790336
gopoma <gordono@unsa.edu.pe>
2023-02-26 18:02:00 -0500

    Populated observation hospital and medico fields

--- src/services/observations.js
+++ src/services/observations.js
@@ -3,6 +3,29 @@ const PacienteService = require("./pacientes");
 const HospitalService = require("./hospitales");
 const handleDBExceptions = require("../helpers/handleDBExceptions");
 
+const populationOptions = [
+    {
+        path: "hospital",
+        model: "hospital",
+        select: "services",
+        populate: {
+            path: "_id",
+            model: "usuario",
+            select: "nombre direccion"
+        }
+    },
+    {
+        path: "medico",
+        model: "medico",
+        select: "-hospital",
+        populate: {
+            path: "_id",
+            model: "usuario",
+            select: "nombre"
+        }
+    }
+];
+
 class ObservationService {
     async create(idMedico, data) {
         const pacienteService = new PacienteService();
@@ -59,19 +82,19 @@ class ObservationService {
     }
 
     async #getRelatedToPaciente(idPaciente) {
-        const observations = await ObservationModel.find({ paciente: idPaciente });
+        const observations = await ObservationModel.find({ paciente: idPaciente }).populate(populationOptions);
 
         return observations;
     }
 
     async #getRelatedToMedico(idMedico) {
-        const observations = await ObservationModel.find({ medico: idMedico });
+        const observations = await ObservationModel.find({ medico: idMedico }).populate(populationOptions);
 
         return observations;
     }
 
     async #getRelatedToHospital(idHospital) {
-        const observations = await ObservationModel.find({ hospital: idHospital });
+        const observations = await ObservationModel.find({ hospital: idHospital }).populate(populationOptions);
 
         return observations;
     }
